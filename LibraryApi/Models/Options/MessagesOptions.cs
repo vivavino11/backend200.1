9cf54217696dfe0a6e29a048ed348c731afd0f8e
vivavino11 <ctuttle1@kent.edu>
2021-01-27 15:53:18 -0500

    EOD check in from training

--- LibraryApi/Models/Options/MessagesOptions.cs
+++ LibraryApi/Models/Options/MessagesOptions.cs
@@ -7,5 +7,8 @@ namespace LibraryApi.Models.Options
 {
     public class MessagesOptions
     {
+        public string SectionName = "Messages";
+        public string CacheMessage { get; set; }
+        public string SomeOtherMessage { get; set; }
     }
 }
--- LibraryApi/Startup.cs
+++ LibraryApi/Startup.cs
@@ -1,5 +1,6 @@
-using AutoMapper;
+﻿using AutoMapper;
 using LibraryApi.Domain;
+using LibraryApi.Models.Options;
 using LibraryApi.Profiles;
 using LibraryApi.Services;
 using Microsoft.AspNetCore.Builder;
@@ -25,6 +26,10 @@ namespace LibraryApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var configForMessages = new MessagesOptions();
+            services.Configure<MessagesOptions>(
+                Configuration.GetSection(configForMessages.SectionName)
+            );
             services.AddTransient<IFormatNames, InformalFormatters>();
             services.AddScoped<ILookupBooks, EfSqlBooks>();
             services.AddScoped<IBookCommands, EfSqlBooks>();
